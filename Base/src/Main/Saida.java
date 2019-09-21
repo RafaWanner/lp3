607e7dd071f527cc6735897ac9e99168a02ddbe9
RafaWanner <46364808+RafaWanner@users.noreply.github.com>
2019-09-21 16:47:31 -0300

    Update Saida.java

--- Base/src/Main/Saida.java
+++ Base/src/Main/Saida.java
@@ -4,19 +4,22 @@ import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 public class Saida {
-    
-    public void imprimirNumeroDouble(double num, String msg) {
 
-        DecimalFormat df = new DecimalFormat("###,##0.00");
+	public void printDouble(double num, String msg) {
+		DecimalFormat df = new DecimalFormat("###,##0.00");
+		JOptionPane.showMessageDialog(null, msg + ": " + df.format(num));
+	}
 
-        JOptionPane.showMessageDialog(null, msg + ": " + df.format(num));
+	public void printString(String msg) {
+		JOptionPane.showMessageDialog(null, msg);
+	}
 
-    }
+	public void printInt(int n, String msg) {
+		JOptionPane.showMessageDialog(null, msg + " : " + n);
+	}
 
-    public void imprimirAvisoString(String msg) {
-
-        JOptionPane.showMessageDialog(null, msg);
-
-    }
+	public void printFiveInt(int n1, int n2, int n3, int n4, int n5, String msg) {
+		JOptionPane.showMessageDialog(null, msg + " : " + n1 + ", " + n2 + ", " + n3 + ", " + n4 + ", " + n5);
+	}
 
 }
